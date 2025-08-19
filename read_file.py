12a1fb02f960280bac77ab9d31960cfa1037152e
Szybet <53944559+Szybet@users.noreply.github.com>
2025-08-19 22:19:11 +0200

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,70 +1,333 @@
-<!DOCTYPE html>
+#!/usr/bin/env python
+# Same licence as
+# https://github.com/smaeul/linux/blob/rk35/ebc-drm-v5/drivers/gpu/drm/ \
+#   drm_epd_helper.c
+# Author: Maximilian Weigand https://github.com/m-weigand/mw_pinenote_misc/tree/main/waveforms
+import ctypes
+import struct
+# import matplotlib.pylab as plt
 
-<html>
-<head>
-    <title>Checking you are not a bot</title>
-    <link rel="stylesheet" href="/.well-known/.git.gammaspectra.live/git/go-away/cmd/go-away/assets/static/anubis/style.css?cacheBust=rQVE1YPFzzs1b7gv7iL-ZQ"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="referrer" content="origin"/>
-    
-    <meta content="0; url=/.well-known/.git.gammaspectra.live/git/go-away/cmd/go-away/challenge/meta-refresh/verify-challenge?__goaway_challenge=meta-refresh&amp;__goaway_id=4bd3e248044dc76d2bc89d7324b24347&amp;__goaway_redirect=%2F~hrdl%2Fpinenote-dist%2Fblob%2Fmain%2Fbin%2Fread_file.py%3F__goaway_challenge%3Dmeta-refresh%26__goaway_id%3D4bd3e248044dc76d2bc89d7324b24347&amp;__goaway_token=01b97353d3e50b4cd86dd1bfc5a5341a25637a22f36645e28f5b66d51434adc1" http-equiv="refresh" />
-    
-    
-    
-</head>
-<body id="top">
-<main>
-    <center>
-        <h1 id="title" class=".centered-div">Checking you are not a bot</h1>
-    </center>
-
-    <div class="centered-div">
-        <img
-                id="image"
-                style="width:100%;max-width:256px;"
-                src="/.well-known/.git.gammaspectra.live/git/go-away/cmd/go-away/assets/static/logo.png?cacheBust=rQVE1YPFzzs1b7gv7iL-ZQ"
-        />
-        
-        <p id="status">Loading challenge <em>meta-refresh</em>...</p>
-        
-        <details>
-            <summary>Why am I seeing this?</summary>
-
-            
-<p>
-	You are seeing this because the administrator of this website has set up <a href="https://git.gammaspectra.live/git/go-away">go-away</a> 
-	to protect the server against the scourge of <a href="https://thelibre.news/foss-infrastructure-is-under-attack-by-ai-companies/">AI companies aggressively scraping websites</a>.
-</p>
-<p>
-	Mass scraping can and does cause downtime for the websites, which makes their resources inaccessible for everyone.
-</p>
-<p>
-	Please note that some challenges requires the use of modern JavaScript features and some plugins may disable these.
-	Disable such plugins for this domain (for example, JShelter) if you encounter any issues.
-</p>
-
-        </details>
-
-        
-
-        
-
-        <p><small>If you have any issues contact the site administrator and provide the following Request Id: <em>4bd3e248044dc76d2bc89d7324b24347</em></small></p>
-    </div>
+import numpy as np
 
 
-    <footer>
-        <center>
-            <p>
-                Protected by <a href="https://git.gammaspectra.live/git/go-away">go-away</a> :: Request Id <em>4bd3e248044dc76d2bc89d7324b24347</em>
+class waveform_file(object):
+    def __init__(self, filename):
+        self.data = open(filename, 'rb').read()
+        self.index = 0
 
-                
-            </p>
-        </center>
-    </footer>
+        self._header = None
+        self._lut_offsets = None
 
+    def get(self, nr_bytes):
+        """Return the next nr_bytes bytes"""
+        subdata = self.data[self.index: self.index + nr_bytes]
+        self.index += nr_bytes
+        return subdata
 
-    
-</main>
-</body>
-</html>
+    def header(self):
+        if self._header is None:
+            self._header = self._read_header()
+        return self._header
+
+    def _read_header(self):
+        header = {
+            'checksum': struct.unpack('<I', self.get(4))[0],
+            'file_size': struct.unpack('<I', self.get(4))[0],
+            'serial': struct.unpack('<I', self.get(4))[0],
+            'run_type': struct.unpack('B', self.get(1))[0],
+            'fpl_platform': struct.unpack('B', self.get(1))[0],
+            # __le16          fpl_lot;        // 0x0e
+            'fpl_lot': struct.unpack('<H', self.get(2))[0],
+            # u8          mode_version;       // 0x10
+            'mode_version': struct.unpack('B', self.get(1))[0],
+            # u8          wf_version;     // 0x11
+            'wf_version': struct.unpack('B', self.get(1))[0],
+            # u8          wf_subversion;      // 0x12
+            'wf_subversion': struct.unpack('B', self.get(1))[0],
+            # u8          wf_type;        // 0x13
+            'wf_type': struct.unpack('B', self.get(1))[0],
+            # u8          panel_size;     // 0x14
+            'panel_size': struct.unpack('B', self.get(1))[0],
+            # u8          amepd_part_number;  // 0x15
+            'amepd_part_number': struct.unpack('B', self.get(1))[0],
+            # u8          wf_rev;         // 0x16
+            'wf_rev': struct.unpack('B', self.get(1))[0],
+            # u8          frame_rate_bcd;     // 0x17
+            'frame_rate_bcd': struct.unpack('B', self.get(1))[0],
+            # u8          frame_rate_hex;     // 0x18
+            'frame_rate_hex': struct.unpack('B', self.get(1))[0],
+            # u8          vcom_offset;        // 0x19
+            'vcom_offset': struct.unpack('B', self.get(1))[0],
+            # u8          unknown[2];     // 0x1a
+            'unknown_1': struct.unpack('B', self.get(1))[0],
+            'unknown_2': struct.unpack('B', self.get(1))[0],
+            # struct pvi_wbf_offset   xwia;           // 0x1c
+            # struct pvi_wbf_offset {
+            #     u8          b[3];
+            # };
+            'xwia_1': struct.unpack('B', self.get(1))[0],
+            'xwia_2': struct.unpack('B', self.get(1))[0],
+            'xwia_3': struct.unpack('B', self.get(1))[0],
+            # u8          cs1;            // 0x1f
+            'cs1': struct.unpack('B', self.get(1))[0],
+            # struct pvi_wbf_offset   wmta;           // 0x20
+            'wmta_1': struct.unpack('B', self.get(1))[0],
+            'wmta_2': struct.unpack('B', self.get(1))[0],
+            'wmta_3': struct.unpack('B', self.get(1))[0],
+            # u8          fvsn;           // 0x23
+            'fvsn': struct.unpack('B', self.get(1))[0],
+            # u8          luts;           // 0x24
+            'luts': struct.unpack('B', self.get(1))[0],
+            # u8          mode_count;     // 0x25
+            # note: we need to add one to get the actual number of modes stored
+            # in the file (apprently INIT is not counted here
+            'mode_count': struct.unpack('B', self.get(1))[0] + 1,
+            # u8          temp_range_count;   // 0x26
+            # note: this one is also off by one
+            'temp_range_count': struct.unpack('B', self.get(1))[0] + 1,
+            # u8          advanced_wf_flags;  // 0x27
+            'advanced_wf_flags': struct.unpack('B', self.get(1))[0],
+            # u8          eb;         // 0x28
+            'eb': struct.unpack('B', self.get(1))[0],
+            # u8          sb;         // 0x29
+            'sb': struct.unpack('B', self.get(1))[0],
+            # u8          reserved[5];        // 0x2a
+            'reserved_1': struct.unpack('B', self.get(1))[0],
+            'reserved_2': struct.unpack('B', self.get(1))[0],
+            'reserved_3': struct.unpack('B', self.get(1))[0],
+            'reserved_4': struct.unpack('B', self.get(1))[0],
+            'reserved_5': struct.unpack('B', self.get(1))[0],
+            # u8          cs2;            // 0x2f
+            'cs2': struct.unpack('B', self.get(1))[0],
+            # u8          temp_range_table[]; // 0x30
+            # TODO
+        }
+        # N temperature ranges require N+1 delimiters
+        nr_temps = header['temp_range_count'] + 1
+        header['temperatures'] = struct.unpack(
+            '{}B'.format(nr_temps), self.get(nr_temps)
+        )
+        return header
+
+    def get_lut_offsets(self):
+        if self._lut_offsets is None:
+            self._lut_offsets = self._get_lut_offsets()
+        return self._lut_offsets
+
+    def _get_lut_offsets(self):
+        """
+
+        """
+        header = self.header()
+
+        def get_offset(bits):
+            return bits[0] | bits[1] << 8 | bits[2] << 16
+
+        def check(bits):
+            # checksum check of offset pointer
+            return ctypes.c_ubyte(bits[0] + bits[1] + bits[2]).value == bits[3]
+
+        # mode table
+        offset = header[
+            'wmta_1'
+        ] | header['wmta_2'] << 8 | header['wmta_3'] << 16
+
+        self._base_offset = offset
+
+        lut_offsets = []
+
+        mode_offsets = []
+        for i in range(0, header['mode_count']):
+            mode_table_offset = struct.unpack(
+                '4B', self.data[offset:offset + 4])
+            assert check(
+                mode_table_offset
+            ), 'mode {} problem {}'.format(i, mode_table_offset)
+            print(mode_table_offset, get_offset(mode_table_offset[0:3]))
+            # mode_offsets += [get_offset(mode_table_offset[0:3])]
+            # extract temperature table
+            temp_table = get_offset(mode_table_offset[0:3])
+            print('temp_table offset', temp_table)
+            mode_offsets += [temp_table]
+            lut_offsets.append({})
+            for j in range(0, header['temp_range_count']):
+                temp_offset = struct.unpack(
+                    '4B', self.data[temp_table:temp_table + 4]
+                )
+                assert check(
+                    temp_offset
+                ), 'temp problem: {} {} {}'.format(i, j, temp_offset)
+                print('    ', temp_offset, get_offset(temp_offset[0:3]))
+                lut_offsets[-1][header['temperatures'][j]] = get_offset(
+                    temp_offset[0:3]
+                )
+
+                temp_table += 4
+            offset += 4
+        self._mode_offsets = mode_offsets
+        return lut_offsets
+
+    def decode_data(self, subdata):
+        """Decode compressed data. Do not split the bytes into phase
+        polarisation information.
+        """
+        token_repeat = int('fc', 16)
+        token_end = int('ff', 16)
+
+        index = 0
+        data_long = []
+        repeat_mode = True
+        while index < len(subdata):
+            token = subdata[index]
+            if token == token_end:
+                index += 1
+                break
+
+            # print(index, data)
+            if token == token_repeat:
+                repeat_mode = not repeat_mode
+                index += 1
+                token = subdata[index]
+
+            if repeat_mode:
+                nr = subdata[index + 1]
+                index = index + 1
+                for i in range(nr + 1):
+                    data_long += [token]
+            else:
+                data_long += [token]
+            index += 1
+
+        return data_long, index
+
+    def split_bytes_into_polarisations(self, byte_data):
+        polarisations = []
+        for a in byte_data:
+            p1 = a & 3
+            p2 = (a >> 2) & 3
+            p3 = (a >> 4) & 3
+            p4 = (a >> 6) & 3
+            polarisations += [p1, p2, p3, p4]
+        return polarisations
+
+    def encode_long_data(self, data_long):
+        # re-encode a long byte stream
+
+        token_repeat = int('fc', 16)
+        token_end = int('ff', 16)
+        data_rec = []
+        index = 0
+
+        def look_ahead(data, index_start):
+            if data[index_start] == data[index_start + 1]:
+                repeat = True
+                index = index_start
+                while(
+                        index < len(data) - 1 and
+                        data[index] == data[index + 1]):
+                    index += 1
+                return repeat, index - index_start
+            else:
+                repeat = False
+                index = index_start
+                while(
+                        index < len(data) - 1 and
+                        data[index] != data[index + 1]):
+                    index += 1
+                return repeat, index - index_start
+
+        # tests
+        # print('look 1', look_ahead([1, 1, 1, 2, 3, 4], 0))
+        # print('look 2', look_ahead([1, 1, 2, 3, 4, 4], 0))
+        # print('look 3', look_ahead([1, 1, 2, 3, 4, 4], 1))
+
+        repeat_mode = True
+        while(index < len(data_long[0:])):
+            repeat, count = look_ahead(data_long, index)
+            # print('look ahead', repeat, count)
+
+            # we need to decide if its worthwhile to change the state
+            if not repeat and repeat_mode:
+                print('Evaluating state')
+                if count > 1:
+                    print('   deactivating at index', index, len(data_rec))
+                    repeat_mode = False
+                    data_rec += [token_repeat]
+
+            if repeat and not repeat_mode:
+                if count >= 2:
+                    print('   activating at index', index, len(data_rec))
+                    repeat_mode = True
+                    data_rec += [token_repeat]
+
+            # do we have repeating entries
+            if repeat:
+                if repeat_mode:
+                    counts = [255] * int(count / 255) + [count % 255]
+                    for subcount in counts:
+                        data_rec += [data_long[index], subcount]
+                else:
+                    for i in range(1 + count):
+                        data_rec += [data_long[index]]
+
+                index += count
+            else:
+                # no repetition ahead
+
+                # do we need to switch?
+                if repeat_mode:
+                    data_rec += [data_long[index], 0]
+                else:
+                    data_rec += [data_long[index]]
+            index += 1
+            # break
+            # if len(data_rec) > 115:
+            #     break
+        data_rec += [token_end]
+
+        # check
+        # for nr, (orig, rec) in enumerate(zip(data_orig, data_rec)):
+        #     if orig != rec:
+        #         print('Error at index: ', nr)
+
+        # print(list(data_orig[len(data_rec) - 10: len(data_rec) + 10]))
+        # print(data_rec[-10:])
+        # print(data_long[index - 10: index + 10])
+        return data_rec
+
+    def get_lut(
+            self, wf, temperature, split_bytes=True, return_compressed=False):
+        """Return the decompressed 1D lut as stored in the file (i.e. 32 x 32
+        levels).
+
+        Parameters
+        ----------
+        wf : int
+            Index of waveform. Must lie in range [0:header['mode_count']]
+        temp: int
+            Temperature to retrieve lut at. See header['temperatures'] for
+            valid values
+        split_bytes: bool, True
+            If true, split each byte so we get individual polarisation values
+        return_compressed: bool, False
+            if True, also return the compressed byte stream of the lut
+
+        Returns
+        -------
+        """
+        header = self.header()
+        assert wf in list(range(0, header['mode_count'])), \
+            "wf index out of range"
+        assert temperature in header['temperatures'], \
+            "temperature not valid"
+
+        lut_offsets = self.get_lut_offsets()
+        # could be more restricted
+        subdata = self.data[lut_offsets[wf][temperature]:]
+        data_long, index = self.decode_data(subdata)
+        data_orig = subdata[:index]
+        if split_bytes:
+            data_long = self.split_bytes_into_polarisations(data_long)
+        if return_compressed:
+            return data_long, data_orig
+        return data_long
